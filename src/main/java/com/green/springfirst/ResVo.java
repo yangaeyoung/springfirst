224d2168e79dc742baf7ea92e7c474822b8974fe
aeyoung <aeyoung0325@naver.com>
2023-11-10 13:14:12 +0900

    23.11.10.

--- src/main/java/com/green/springfirst/ResVo.java
+++ src/main/java/com/green/springfirst/ResVo.java
@@ -3,11 +3,11 @@ package com.green.springfirst;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+//@NoArgsConstructor 기본 생성자를 만들어 줌
 public class ResVo {
     private int result;
-
-
 }
--- src/main/java/com/green/springfirst/board/BoardController.java
+++ src/main/java/com/green/springfirst/board/BoardController.java
@@ -11,13 +11,13 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController // 응답 json이 기본 // controller는 html이 기본 응답
-@RequiredArgsConstructor //추가하면 BoardService의 객체 주소값이 들어오게 됨
+@RequiredArgsConstructor // final이 붙은 거에 자동으로 생성자를 만듦. BoardService의 객체 주소값이 들어오게 됨, final 꼭 붙여주면 되...
+//@RequestMapping("/board") 하면 현재 어노테이션 옆에 있는 ("/board) 지워줘도 됨
 public class BoardController {
 
     private final BoardService service;
 
     @PostMapping("/board")// 주소값이 같으나 요청이 get으로 들어왔을 때와 post로 들어왔을 때 구분이 될 수 있음
-    @ResponseBody // controller 였으면 이걸 사용
     public ResVo insBoard(@RequestBody BoardInsDto dto){ // json형태로 매핑해서 응답
         int result = service.insBoard(dto);
         return new ResVo(result);
